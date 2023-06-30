d00e3312b1b83afed8b0f20617934a68e3aaf5ea
AleksKostadinov <kostadinov12@gmail.com>
2023-06-30 15:47:58 +0300

    Update Note

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import notes from '../assets/data';
-import { useParams, Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+// import notes from '../assets/data';
+import { useParams, useNavigate } from 'react-router-dom';
 import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg';
 
-const NotePage = () => {
-    const params = useParams();
 
-    let noteId = params.id
-    let note = notes.find(note => note.id === Number(noteId))
+const NotePage = () => {
+
+    const params = useParams();
+    const noteId = params.id
+    const baseUrl = `http://localhost:8000/notes/${noteId}`
+    // let note = notes.find(note => note.id === Number(noteId))
+    const [note, setNote] = useState(null)
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        getNote()
+    }, [noteId]) //when is changing noteId to rerun useEffect
+
+    const getNote = () => {
+        fetch(baseUrl)
+        .then(res => res.json())
+        .then(result => {
+            setNote(result)
+        } )
+    }
+
+    // const getNote = async () => {
+    //     const response = await fetch(`http://localhost:8000/notes/${noteId}`)
+    //     const result = await response.json()
+    //     setNote(result)
+    // }
+
+    const onChange = (e) =>{
+        setNote({
+            ...note,
+            'body': e.target.value
+        });
+    }
+
+    const updateNote = async () => {
+        await fetch(`http://localhost:8000/notes/${noteId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({...note, 'updated': new Date()})
+        })
+    }
+
+    const handleSubmit = () => {
+        updateNote()
+        navigate('/')
+    }
 
     return (
         <div className='note'>
             <div className="note-header">
                 <h3>
-                    <Link to="/">
-                        <ArrowLeft />
-                    </Link>
+                    <ArrowLeft onClick={handleSubmit}/>
                 </h3>
             </div>
 
-            <textarea value={note?.body}>
-                
+            <textarea onChange={onChange} placeholder="Edit note" value={note?.body}>
+
             </textarea>
         </div>
     )
--- src/pages/NotesListPage.js
+++ src/pages/NotesListPage.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import notes from '../assets/data'
+import React, { useEffect, useState } from 'react';
+// import notes from '../assets/data'
 import ListItem from '../components/ListItem';
 
 const NotesListPage = () => {
+
+    const [notes, setNotes] = useState([])
+
+    useEffect(() => {
+        getNotes()
+    }, [])
+
+    const getNotes = () => {
+        fetch('http://localhost:8000/notes/')
+        .then(res => res.json())
+        .then(result => {
+            setNotes(result)
+        } )
+    }
+
+    // const getNotes = async () => {
+    //     const response = await fetch('http://localhost:8000/notes/')
+    //     const data = await response.json()
+    //     setNotes(data)
+    // }
+
     return (
         <div className='notes'>
             <div className='notes-header'>
